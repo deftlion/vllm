e5b59e778d839fa12d1d8f8c8dc5a84a6ce0ee21
deftlion <163032230+deftlion@users.noreply.github.com>
2024-12-06 01:32:02 -0800

    - slim build down targeting colab env - enable pytorch nightly installation in ci

--- .github/workflows/scripts/pytorch-install.sh
+++ .github/workflows/scripts/pytorch-install.sh
@@ -6,8 +6,12 @@ cuda_version=$3
 
 # Install torch
 $python_executable -m pip install numpy pyyaml scipy ipython mkl mkl-include ninja cython typing pandas typing-extensions dataclasses setuptools && conda clean -ya
-$python_executable -m pip install torch==${pytorch_version}+cu${cuda_version//./} --extra-index-url https://download.pytorch.org/whl/cu${cuda_version//./}
 
+if [[ $pytorch_version == "nightly" ]]; then
+  $python_executable -m pip install torch --pre --upgrade --index-url https://download.pytorch.org/whl/nightly/cu121
+else
+  $python_executable -m pip install torch==${pytorch_version}+cu${cuda_version//./} --extra-index-url https://download.pytorch.org/whl/cu${cuda_version//./}
+fi
 # Print version information
 $python_executable --version
 $python_executable -c "import torch; print('PyTorch:', torch.__version__)"
